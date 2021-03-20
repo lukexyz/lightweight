100cd0d0df110625d034e072f525c1c5cc5f39e4
lukexyz <lukedwoods@gmail.com>
2021-03-20 10:25:05 +0000

    rename script

---
+++ data/rename_files.py
@@ -0,0 +1,6 @@
+﻿import os
+os.getcwd()
+collection = "C:/Python/lightweight/data/01_luke_premium"
+for i, filename in enumerate(os.listdir(collection)):
+    os.rename("C:/Python/lightweight/data/01_luke_premium/" + filename,
+              "C:/Python/lightweight/data/01_luke_premium/" + "01_luke_premium" + filename)

f8d20f73939cc11b1a1a79bf69d58a52475ae415
lukexyz <lukedwoods@gmail.com>
2021-03-28 20:17:02 +0100

    add nn to opencv main loop

--- lightweight.py
+++ lightweight.py
@@ -1,18 +1,30 @@
 import cv2
-# from glob import glob  
-# from collections import OrderedDict
-# import os, pathlib
 import torch
+import fastai
+from fastai.vision.all import *
 
+from time import sleep
+from rich.console import Console
+
+import os
+import pathlib # Unix -> WindowsPath
+_path = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
 
 from utils.timer import Timer
-print('torch version:', torch.__version__)
+
+print(f' Fastai {fastai.__version__}\nPytorch {torch.__version__}\n OpenCV {cv2.__version__}')
+try:
+  print(f'    GPU {torch.cuda.get_device_name(0)}')
+  torch.cuda.device(0)
+except: print("No GPU detected")
+
 
 # ================== OpenCV Video Capture =================== #
 cap = cv2.VideoCapture(0)
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-print('Frame width:', frame_width)
+print('\nFrame width:', frame_width)
 print('Frame height:', frame_height)
 print('Capture frame rate:', cap.get(cv2.CAP_PROP_FPS))
 font = cv2.FONT_HERSHEY_SIMPLEX
@@ -22,38 +34,63 @@ _t = {'fps': Timer()}
 _nn = {'fps': Timer()}
 
 
+console = Console()
+tasks = [f"task {n}" for n in range(1, 11)]
+
+with console.status("[bold green]Working on tasks...") as status:
+    while tasks:
+        task = tasks.pop(0)
+        sleep(1)
+        console.log(f"{task} complete")
+
+
+
+def load_lightweight():
+    # load weights 
+    print('\nloading model ...')
+    net = load_learner(Path('models/lw_nb02.pkl'))
+    print('finished loading')
+    return net
+
+
+# Initialize facesboxes model
+net = load_lightweight()
+
+
 while True:
     _t['fps'].tic()
 
     # Capture frame and mirror horizontal
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
-
-    # Crop image to central/top area
-    # (optimises foward-pass speed and overall FPS)
-    c1 = frame_height * 1  # % Keep from top
-    c2 = frame_width * 0.2   # % removed from each side
-    y1, y2 = 0, int(c1)
-    x1, x2 = int(c2), int(frame_width - c2)
-    frame = cv2.resize(frame[y1:y2, x1:x2], None, fx=0.8, fy=0.8)
+    x0, y0 = frame.shape[0], frame.shape[1]
 
 
-    outframe = cv2.resize(frame, None, fx=1.2, fy=1.2)
+    # nn
+    preds = net.predict(frame)
+    print(f'\t▷ {preds[0]} ({max(preds[2]):0.4f})')
+
+    # Resize for output
+    frame = cv2.resize(frame, None, fx=1.5, fy=1.5)
 
     # Render FPS
     _t['fps'].toc()
     fps = 'FPS: {:.3f}'.format(1 / _t['fps'].diff)
-    cv2.putText(outframe, fps, (11, 15), font, 0.35, (255, 255, 255), 1, cv2.LINE_AA)
+    cv2.putText(frame, fps, (11, 15), font, 0.35, (255, 255, 255), 1, cv2.LINE_AA)
     res = 'NN-res: {}x{}'.format(frame.shape[0], frame.shape[1])
-    cv2.putText(outframe, res, (11, 33), font, 0.35, (255, 255, 255), 1, cv2.LINE_AA)
+    cv2.putText(frame, res, (11, 33), font, 0.35, (255, 255, 255), 1, cv2.LINE_AA)
 
     
     # Display headtracking frame
-    cv2.imshow('Headtracking', outframe)
+    cv2.imshow('Webcam: lightweight', frame)
+
+
 
 
     k = cv2.waitKey(1) & 0xFF
     if k == 27:  # ESC TO QUIT
+        break
+    if k == ord('q'):  # q TO QUIT
         break
     elif k == ord('r'):   # RESET ORIENTATION
         print('Key press: R')

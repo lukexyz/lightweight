9b760a1784ea79035a2eae41db8f241cd1b9ecfa
lukexyz <lukedwoods@gmail.com>
2021-03-28 20:15:04 +0100

    rich print example

---
+++ utils/rich.py
@@ -0,0 +1,11 @@
+from time import sleep
+from rich.console import Console
+
+console = Console()
+tasks = [f"task {n}" for n in range(1, 11)]
+
+with console.status("[bold green]Working on tasks...") as status:
+    while tasks:
+        task = tasks.pop(0)
+        sleep(1)
+        console.log(f"{task} complete")
